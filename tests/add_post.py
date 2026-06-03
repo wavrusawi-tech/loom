0746a9e1e875be3930f02a5ca484c9111bdd6518
William <wavrusawi@gmail.com>
2026-06-03 17:01:58 -0500

    Updated tests

--- tests/add_post.py
+++ tests/add_post.py
@@ -1,3 +1,6 @@
 import loom
 
-post_config = loom.classes.blog.PostConfig()
+post_config = loom.classes.blog.PostConfig(False)
+post_text_config = loom.classes.blog.PostConfig.PostTextConfig(26, 20, 14, 12, 11)
+
+post = loom.classes.blog.Post(post_text_config, post_config)
